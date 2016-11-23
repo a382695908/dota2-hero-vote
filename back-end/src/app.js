4b350df34254da723577b36e4281a086df149a74
yyj <yyjazsf@live.cn>
2016-11-23 22:38:26 +0800

    去除babel

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -14,7 +14,10 @@ app.set('port', process.env.PORT || 3000)
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// todo add api
+
 
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port ' + app.get('port'));
