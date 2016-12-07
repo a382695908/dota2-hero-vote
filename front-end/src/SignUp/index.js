67b4ed8f804cb4f6bb798bbeb35145ca910897ea
Ivan Ying <yyjazsf@live.cn>
2016-12-07 16:36:31 +0800

    fixed spelling mistake 'from' -> 'form',and add Event

--- front-end/src/SignUp/index.js
+++ front-end/src/SignUp/index.js
@@ -6,27 +6,55 @@ import {Link} from 'react-router';
 import './sign-up.less';
 
 class SignUp extends Component {
+
+  constructor(props) {
+    super();// props
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      email     : '',
+      password  : '',
+      confirmpwd: ''
+    }
+  }
+
+  handleChange() {
+    // console.log(this.refs)
+    // add validation
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log('email', this.refs.email.value);
+    console.log('password', this.refs.password.value);
+    console.log('confirmpwd', this.refs.confirmpwd.value);
+  }
+
   render() {
     return (
       <div className="page-sign-up view-center-box">
         <h5>join and vote</h5>
-        <from className="" method="post" autoComplete="off" noValidate>
+        <form noValidate onSubmit={this.handleSubmit}>
           <div className="mdl-grid">
             <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-              <input className="mdl-textfield__input" type="text" id="email"/>
+              <input className="mdl-textfield__input"
+                     type="text" id="email"
+                     onChange={this.handleChange}
+                     ref="email"/>
               <label className="mdl-textfield__label" htmlFor="email">email</label>
             </div>
             <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-              <input className="mdl-textfield__input" type="password" id="password"/>
-              <label className="mdl-textfield__label" htmlFor="email">password</label>
+              <input className="mdl-textfield__input" type="password" id="password" ref="password"/>
+              <label className="mdl-textfield__label" htmlFor="password">password</label>
             </div>
             <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-              <input className="mdl-textfield__input" type="password" id="confirmpwd"/>
+              <input className="mdl-textfield__input" type="password" id="confirmpwd" ref="confirmpwd"/>
               <label className="mdl-textfield__label" htmlFor="confirmpwd">confirm password</label>
             </div>
           </div>
           <div className="from-action mdl-grid">
-            <Link to="/login" className="mdl-button mdl-js-button mdl-button--fab mdl-button--icon" >
+            <Link to="/login" className="mdl-button mdl-js-button mdl-button--fab mdl-button--icon">
               <i className="material-icons">keyboard_arrow_left</i>
             </Link>
             <input type="submit"
@@ -34,10 +62,15 @@ class SignUp extends Component {
                    value="Continue"/>
           </div>
 
-        </from>
+        </form>
       </div>
     );
   }
 }
+
+// 联合登录
+SignUp.propTypes = {
+  githubId: React.PropTypes.string
+};
 
 export default SignUp;
