e834dbd41c180394a1d6cea22d194c3b35370c60
yingyujia <yyjazsf@live.cn>
2016-12-25 13:07:33 +0800

    base frame

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,29 +1,60 @@
 /**
- *
+ * server
  */
 
-const express = require('express')
-const path = require('path')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
-const router = require('./config/router');
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import qs from 'qs';
+import mongoose from 'mongoose';
+import config from './config';
+import API from './api';
+
 const app = express();
-
-app.set('port', process.env.PORT || 3000)
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.set('port', config.port);
 // app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'));
+app.use(cookieParser());//todo remove
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// todo add api
+mongoose.connect(config.database);
 
-router(app);
-console.log('>>>>>>>>>>>>>>>>>>>>>>',router);
-app.use(function(req,res,next){
-	console.log('app.use');
+app.use('/api', API);
+
+/**
+ * catch 404 and forward to error handler
+ * 到达后面的都是未处理的路由
+ * todo 写日志
+ */
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.use(function (err, req, res, next) {
+  if (err.status = 404) {
+    res
+      .status(404)
+      .json({
+        code: 404,
+        msg: '没有这个接口'
+      });
+  }
+  else {
+    res
+      .status(500)
+      .send(`server error,code:${err.status || 500}`);
+  }
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get('port'), (err) => {
+  if(err){
+    console.log(err.message);
+  }
+  else{
+    console.log(`Express server listening on port ${app.get('port')}`);
+  }
 })
-
---
+++ back-end/src/config/index.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+let config = {
+
+  debug: true,
+
+  port: 3000,
+
+  secret: 'test_secret',
+  //mongodb
+  database: 'mongodb://localhost/vote-hero-dota2',
+
+}
+export default config;
