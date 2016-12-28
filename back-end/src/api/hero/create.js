b31833a68a7f280f0e358dacfd55b7efe53fbbf7
Ivan Ying <yyjazsf@live.cn>
2016-12-28 18:05:43 +0800

    hero CU

--- back-end/src/api/hero/create.js
+++ back-end/src/api/hero/create.js
@@ -1,7 +1,29 @@
 /**
- * 
+ * add new hero
+ * http://dota2.gamepedia.com/Heroes
  */
 
-export default function (req, res) {
+
+import Hero from '../../models/hero';
+
+export default function (req, res, next) {
+
+  new Hero({
+    name: req.body.name,
+    enName: req.body.enName,
+    nick: req.body.nick,
+    imgUrl: req.body.imgUrl || 'images/hero/default.jpg',
+    type: req.body.type,
+    vote: req.body.vote || 0,
+  })
+    .save()
+    .then(data => {
+      res.json({
+        success: true,
+      });
+    })
+    .catch(function (err) {
+      next(err);
+    });
 
 }
--- back-end/src/api/hero/update.js
+++ back-end/src/api/hero/update.js
@@ -1,7 +1,13 @@
 /**
- * 
+ *
  */
 
-export default function (req, res) {
+import Hero from '../../models/hero';
 
+export default function (req, res, next) {
+  Hero.update({
+    _id: req.params.id
+  },{
+
+  })
 }
