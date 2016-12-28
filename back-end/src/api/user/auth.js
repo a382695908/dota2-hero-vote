efa27eee6cb9052c63f9504bfe2daa0e5b6a2c1e
Ivan Ying <yyjazsf@live.cn>
2016-12-28 18:05:49 +0800

    auth

--- back-end/src/api/user/auth.js
+++ back-end/src/api/user/auth.js
@@ -3,29 +3,47 @@
  */
 
 import jwt from 'jsonwebtoken';
+import utils from '../../utils';
 import config from '../../config';
 
 export default function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-  console.log(req.ip, req.ips);
+  console.log(req.ip);
 
-  if (req.headers.token) {
-
-    jwt.verify(req.headers.token, config.secret, (err, decoded) => {
-      console.log(decoded);
-
-      if (err) {
-        /*
-         err = {
-         name: 'TokenExpiredError',
-         message: 'jwt expired',
-         expiredAt: 1408621000
-         }
-         */
-      }
-    });
+  async function processJwt(err, decoded) {
+    let auth = await utils.redis.get(req.headers.authorization);
+    if(!auth){
+      res.status(401)
+        .send({
+        success: false,
+        msg: `token无效或过期:${req.headers.authorization}`,
+      });
+    }
+    else if (err) {
+      res.status(401).send({
+        success: false,
+        msg: `无效的token:${err.message}`,
+      });
+      /*
+       err = {
+       name: 'TokenExpiredError',
+       message: 'jwt expired',
+       expiredAt: 1408621000
+       }
+       */
+    }
+    req.auth = JSON.parse(auth);
+    next();
   }
-  next();
+
+  if (req.headers.authorization) {
+    jwt.verify(req.headers.authorization, config.secret, processJwt);
+  }
+  else{
+    res.status(403).send('forbidden!');
+  }
 }
+
+
