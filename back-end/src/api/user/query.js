32eb243e709557ff4e2d5ec4f281ec1324796d6d
Ivan Ying <yyjazsf@live.cn>
2016-12-28 18:05:57 +0800

    add user query

--- back-end/src/api/user/query.js
+++ back-end/src/api/user/query.js
@@ -2,26 +2,39 @@
  *
  */
 
+import validator from 'validator';
 import User from '../../models/user';
 import utils from '../../utils';
 
 export function getUserByUsername(username) {
 
-  User.findOne({
-    username: req.body.username,
-  })
-    .then((err, user) => {
-      return user;
-    })
-    .catch(err => {
-
-    })
+  return User.findOne({
+    username: username,
+  });
 }
 
-export default function (req, res, next) {
+export function getUserByUserId(id) {
+  if(!validator.isMongoId(id)){
+    return null;
+  }
+  return User.findOne({
+    _id: id,
+  });
+}
+
+export default async function (req, res, next) {
+
+  const user = await getUserByUserId(req.auth._id);
+  let json = {};
+  if(user){
+    Object.assign(json, user._doc);
+    delete json.__v;
+    delete json.password;
+  }
 
   res.json({
     success: true,
-    msg: '成功'
+    msg: '成功',
+    data: json
   })
 }
