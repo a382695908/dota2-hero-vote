d2b9e53c111016f1b0d3b76b3ca8594d69c6faa6
Ivan Ying <yyjazsf@live.cn>
2016-12-30 15:39:16 +0800

    delete hero

--- back-end/src/api/hero/delete.js
+++ back-end/src/api/hero/delete.js
@@ -1,7 +1,20 @@
 /**
- * 
+ *
  */
 
-export default function (req, res) {
+import Hero from '../../models/hero';
+
+export default function (req, res, next) {
+
+  Hero.remove({
+    _id: req.params.id
+  })
+    .then(data => {
+      res.json({
+        success: true,
+        msg: `英雄${data.name}删除成功`,
+      })
+    })
+    .catch(err => next(err));
 
 }
